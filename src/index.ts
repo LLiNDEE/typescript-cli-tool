b510fee7b2a4251e0dd8ce3e6d00eddb862b3779
LLiNDEE <linus.linde@bahnhof.se>
2022-05-01 20:56:47 +0200

    fix: Add new type project and add project name question

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,33 @@ import { createQuestion } from './utils/Question.js';
 
 const templates = ["Typescript", "NodeJS", "HTML/CSS/JS"];
 
+type project = {
+    type: string | undefined,
+    path: string | undefined,
+    name: string | undefined,
+}
+
 async function index(): Promise<void>
 {
-    const projectpath = await createQuestion({question: "What is the project path?", validation: "folder", defaultValue: "Denna mapp"});
-    console.log(projectpath.answer);
 
-    const typeOfProject = await createQuestion({
-        question: "What type of project do you want to create?",
-        type: "list",
-        choices: templates,
-    })
+    const project: project = {
+        type: undefined,
+        path: undefined,
+        name: undefined,
+    }
 
-    console.log("Type: ", typeOfProject)
+    const typeOfProject = await createQuestion({question: "What type of project do you want to create?", type: "list", choices: templates});
+    project.type = typeOfProject.answer
+
+    console.log(typeOfProject.answer);  
+
+    const projectName = await createQuestion({question: "Enter the name of the project", validation: 'projectName'})
+    project.name = projectName.answer
+
+    const projectpath = await createQuestion({question: "Enter folder name or path", validation: "folder", defaultValue: "Current path", question_type: "project_path"})
+    project.path = projectpath.answer
+    // console.log("Type: ", typeOfProject)
+    console.log(project)
 
 }
 
