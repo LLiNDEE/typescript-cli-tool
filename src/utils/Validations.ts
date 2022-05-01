e7076e844a5aa32a44bfae30c47078b066d60147
LLiNDEE <linus.linde@bahnhof.se>
2022-05-01 20:55:06 +0200

    fix: Add validation for project name

--- src/utils/Validations.ts
+++ src/utils/Validations.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import process from 'process'
 
-export async function validateFolder(path: string): Promise<string>
+export async function validateFolder(path: string): Promise<string | boolean>
 {   
 
     const folder: Promise<boolean> = new Promise((resolve, reject) => {
@@ -41,5 +41,18 @@ export async function validateFolder(path: string): Promise<string>
         })
     });
 
-    return await folder ? `A directory called ${path} already exists` : "Creating directory...";
+    return await folder ? `A directory called ${path} already exists` : true;
+}
+
+export function validateProjectName(name: string): string | boolean
+{   
+
+   if(name.length  === 0) return "Project name can NOT be empty!"
+   if(name.length > 214) return "Project name length CANNOT exceed 214 characters"
+   if(name.toLowerCase() !== name) return "Project name MUST be in lowercase!"
+   if(!name.match(/^[a-z\-0-9]+$/gm)) return "Project name may ONLY contain alphanumeric characters and -"
+   if(name.startsWith("." || name.startsWith("-"))) return "Project name cannot start with . or -"
+   if(name.includes('http') || name.includes('stream') || name.includes('node_modules') || name.includes('favicon.ico')) return "Project name conatins invalid substring"
+
+    return true
 }
